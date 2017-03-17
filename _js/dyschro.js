54f81862fd5f3a4c00dc31b25ef48916e2d1dba3
Roland Rytz <roland.rytz@gmail.com>
2017-03-17 13:51:10 +0100

    Add author and license information

--- _js/dyschro.js
+++ _js/dyschro.js
@@ -1,3 +1,31 @@
+
+/*
+	========================================================================
+
+	Color Blindness Tools
+	
+	A collection of tools for simulating and providing assistance
+	with various forms of color vision deficiency.
+	
+	Copyright (C) 2017 Roland Rytz <roland@draemm.li>
+	Licensed under the GNU Affero General Public License Version 3
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as
+	published by the Free Software Foundation, either version 3 of the
+	License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+	For more information, see:
+	https://draemm.li/various/colorVision/license.txt
+	
+	========================================================================
+ */
+
+
 var image = new Image();
 var imageElement = document.getElementById("image");
 var imageContainer = document.getElementById("imageContainer");
